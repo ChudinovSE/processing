a4d37ea3e714855f8b1c43d7d563872136f8d81f
ChudinovSE <chudinovse@mail.ru>
2021-08-19 14:38:09 +0700

    demo main, fix interpolution

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -1,5 +1,3 @@
-use crate::date::DateRU;
-use crate::date::TimeInterval;
 use crate::volume::Volume;
 use crate::info::TestInfo;
 
@@ -10,15 +8,22 @@ pub enum MethodInter {
 }
 
 fn calc_step (first: &TestInfo, second: &TestInfo) -> (Volume, u32) {
-	let (first_day, first_month, first_year) = first.date.get_date();
-	let (second_day, second_month, second_year) = second.date.get_date();
+	let (_, first_month, first_year) = first.date.get_date();
+	let (_, second_month, second_year) = second.date.get_date();
 
-	let d_day: u32 = 
-			(second_day as u32 - first_day as u32) +
-			(second_month as u32 - first_month as u32)*31 +
-			(second_year - first_year)*31*12;
+	let mut d_month;
+	let mut d_year;
 
-	let mut d_vol = (second.vol - first.vol);
+	if (second_month as i32 - first_month as i32) < 0 {
+		d_month = 1;
+		d_year = 0;
+	} else {
+		d_month = second_month as u32 - first_month as u32;
+		d_year = second_year - first_year;
+	}
+
+	let d_day: u32 = d_month*31 + d_year*31*12;
+	let mut d_vol = second.vol - first.vol;
 	d_vol.mean_vol(d_day as f64);
 
 	(d_vol,	d_day)
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,60 @@
 use std::fs;
 
-//use processing::date::DateRU;
-
 use processing::info::TestInfo;
 use processing::grouping::MethodGroup;
 use processing::grouping::grouping;
 use processing::interpolation::MethodInter;
 use processing::interpolation::interpolation;
 use processing::date::TimeInterval;
-//use processing::grouping;
-//use processing::interpolation;
-
 
 fn main() {
+	// Read and parse file with random info
+    let raw_data = fs::read_to_string("rnd_data.txt").unwrap();
+    let mut pars_data: Vec<TestInfo> = TestInfo::pars_info(&raw_data);
+    println!("Random info:");
+    for i in 0..5 {
+    	println!("{}", pars_data[i].print_line());
+    }
+    println!("..........................");
+    println!("");
 
-    let raw_data = fs::read_to_string("series_data_test.txt").unwrap();
+    // Sort random info
+    pars_data.sort_by(|a, b| a.date.cmp(&b.date));
+    println!("Sort by date:");
+    for i in 0..5 {
+    	println!("{}", pars_data[i].print_line());
+    }
+    println!("..........................");
+    println!("");
 
+    // Read and parse file with series info
+    let raw_data = fs::read_to_string("series_data.txt").unwrap();
     let pars_data: Vec<TestInfo> = TestInfo::pars_info(&raw_data);
-   // pars_data.sort_by(|a, b| a.data.cmp(&b.data));
-
-    let year_data = grouping(&pars_data, MethodGroup::First, TimeInterval::Month);
-   // println!("year_data.len {}", year_data.len());
-    for i in 0..year_data.len() {
-    	println!("[DEBUG] {}", year_data[i].print_line());
+    println!("Series date:");
+    for i in 0..5 {
+    	println!("{}", pars_data[i].print_line());
     }
+    println!("..........................");
+    println!("");
 
-
-    let inter_data = interpolation(&year_data, MethodInter::Line);
-
-    println!("{}", inter_data.len());
-    
-    for i in 0 .. inter_data.len() {
-    	println!("[DEBUG] {}", inter_data[i].print_line());
+    // Aggregate info for months, by first volume
+    let month_data = grouping(&pars_data, 
+    	MethodGroup::First, 
+    	TimeInterval::Month);
+    println!("Aggregate info for months, by first volume:");
+    for i in 0..5 {
+    	println!("{}", month_data[i].print_line());
     }
+    println!("..........................");
+    println!("");
+
+    // Line interpolation info
+    let inter_data = interpolation(&month_data, MethodInter::Line);
+    println!("Line interpolation info:");
+    for i in 0..5 {
+    	println!("{}", inter_data[i].print_line());
+    }
+    println!("..........................");
+    println!("");
+
 }
